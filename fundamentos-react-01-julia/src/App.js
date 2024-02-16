fcb5d80c925eb6a8783c8b397f730aab10f918cc
juliaraujo10 <alvs.july@gmail.com>
2024-02-15 22:41:12 -0300

    Julia: Primeira parte do css do cardBokk

--- fundamentos-react-01-julia/src/App.js
+++ fundamentos-react-01-julia/src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <div className="cardBook">
       <h3> As cavernas de aço</h3>
 
       <p>Isaac Asimov</p>
@@ -14,7 +15,7 @@ function App() {
       <p>
       As Cavernas de Aço, de Isaac Asimov, é o primeiro romance da consagrada Série dos Robôs, uma das mais populares da ficção científica. A história nasceu de um desafio: Asimov queria provar para seu editor que a ficção científica não era limitada e poderia ser incorporada a qualquer gênero literário, inclusive nos dramas policiais.
       </p>
-
+      </div>
     </div>
   );
 }
