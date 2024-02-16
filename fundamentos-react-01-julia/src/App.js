9da7315e29532e99176c72c220e7c29326d88c66
juliaraujo10 <alvs.july@gmail.com>
2024-02-15 22:23:16 -0300

    Julia: Estrutura base do card de livros sem css.

--- fundamentos-react-01-julia/src/App.js
+++ fundamentos-react-01-julia/src/App.js
@@ -1,23 +1,20 @@
 import logo from './logo.svg';
+import cavernas from './livros/cavernas_aco.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3> As cavernas de aço</h3>
+
+      <p>Isaac Asimov</p>
+
+      <img src={cavernas} width={150} alt='Capa do livro As Cavernas de Aço de Isaac Asimov'/>
+
+      <p>
+      As Cavernas de Aço, de Isaac Asimov, é o primeiro romance da consagrada Série dos Robôs, uma das mais populares da ficção científica. A história nasceu de um desafio: Asimov queria provar para seu editor que a ficção científica não era limitada e poderia ser incorporada a qualquer gênero literário, inclusive nos dramas policiais.
+      </p>
+
     </div>
   );
 }
